8fc48ed814c4f5d8609de7cf671a63388dfff0ac
CreamK <51256470+CreamK@users.noreply.github.com>
2021-02-27 18:44:38 +0800

    Update youth.js

--- youth.js
+++ youth.js
@@ -17,7 +17,7 @@ let notifyInterval = $.getdata("notifytimes") || 1; //通知间隔，默认抽�
 let ONCard = $.getdata('zqcard') || "false"; //早起打卡开关
 let s = $.getdata('delay_rotary_zq') || "10"; //转盘延迟时间
 let withdrawcash = $.getdata('zqcash') || 30; //提现金额
-let cardTime = $.getdata('zqtime') || "05"; //打卡时间
+let cardTime = $.getdata('zqtime') || "22"; //打卡时间
 
 // 需获取部分
 let withdrawUrl = $.getdata('cashurl_zq'); //提现地址，可选
@@ -378,7 +378,7 @@ function endCard() {
                 } else if (punchcardend.code == 0) {
                     // TODO .不在打卡时间范围内
                     $.desc += `【早起打卡】${punchcardend.msg}\n`;
-                    // $.log("不在打卡时间范围内")
+                    $.log("不在打卡时间范围内")
                 }
                 resolve()
             })
