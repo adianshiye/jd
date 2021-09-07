e968a46a65b3e8e2ec3466dbbe9101dcca838677
ccwav <ccwav@163.com>
2021-09-07 22:45:40 +0800

    Update jd_CheckCK.js

--- jd_CheckCK.js
+++ jd_CheckCK.js
@@ -184,7 +184,7 @@ if (process.env.CKNOWARNERROR) {
         }
 		
 		if(NoWarnError== "true"){
-			OErrorMessage="NoWarn!";
+			OErrorMessage="";
 		}
 		
         if ($.isNode() && (EnableMessage || DisableMessage || OErrorMessage || CKAlwaysNotify == "true")) {
