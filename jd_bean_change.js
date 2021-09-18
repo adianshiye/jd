234f678eac76d6a4f39988bc4b87bdd1802fd5f2
ccwav <ccwav@163.com>
2021-09-18 23:13:17 +0800

    红包项目为0不显示

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -624,12 +624,16 @@ function redPacket() {
                             $.jdRed = $.jdRed.toFixed(2)
                             $.jdhRed = $.jdhRed.toFixed(2)
                             $.balance = data.balance
-                            $.expiredBalance = ($.jxRedExpire + $.jsRedExpire + $.jdRedExpire).toFixed(2)
+                            $.expiredBalance = ($.jxRedExpire + $.jsRedExpire + $.jdRedExpire).toFixed(2)							
                             $.message += `【红包总额】${$.balance}(总过期${$.expiredBalance})元 \n`;
-                        $.message += `【京喜红包】${$.jxRed}(将过期${$.jxRedExpire.toFixed(2)})元 \n`;
-                        $.message += `【极速红包】${$.jsRed}(将过期${$.jsRedExpire.toFixed(2)})元 \n`;
-                        $.message += `【京东红包】${$.jdRed}(将过期${$.jdRedExpire.toFixed(2)})元 \n`;
-                        $.message += `【健康红包】${$.jdhRed}(将过期${$.jdhRedExpire.toFixed(2)})元 `;
+                        if ($.jxRed>0)
+                            $.message += `【京喜红包】${$.jxRed}(将过期${$.jxRedExpire.toFixed(2)})元 \n`;
+                        if ($.jsRed>0)
+                            $.message += `【极速红包】${$.jsRed}(将过期${$.jsRedExpire.toFixed(2)})元 \n`;
+                        if ($.jdRed>0)
+                            $.message += `【京东红包】${$.jdRed}(将过期${$.jdRedExpire.toFixed(2)})元 \n`;
+                        if ($.jdhRed>0)
+                            $.message += `【健康红包】${$.jdhRed}(将过期${$.jdhRedExpire.toFixed(2)})元 `;
                     } else {
                         console.log(`京东服务器返回空数据`)
                     }
